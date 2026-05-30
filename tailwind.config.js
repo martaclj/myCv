86a1c5f96914fc68578a985f0b3f20db1192792c
martaclj <marlarrad@gmail.com>
2026-05-30 14:48:48 +0200

    cv actual

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* variables globales de style.css */
+        'primary': 'var(--hospital-green)',
+        'secondary': 'var(--very-light-pink)',
+        'bg-base': 'var(--text-input-field)'
+      },
+      fontFamily: {
+        sans: ['Quicksand', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
